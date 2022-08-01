a6ab898e91130efa121ff94f0c84b3d689e00f95
cimpean calin <cimpeancalin@cimpeans-MacBook-Pro.local>
2022-08-01 09:55:53 +0300

    commit message

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
-var clickedTime; var createdTime; var reactionTime; var x,y,h,s,mi,interval,ms;var i=0;
+var clickedTime; var createdTime; var reactionTime; var x,y,h,s,mi,interval,ms;var i=0;var StartGame=0;var q=0;
 createdTime=Date.now();const watch = document.querySelector('#watch');let milliseconds = 0;let timer;
 color = ["rgb(124, 5, 156)","rgb(90, 173, 218)", "rgb(0, 68, 31)", "Red", "Orange", "Yellow ","Pink", "Orange", "Red","rgb(76,60,42)" ];
-
+var bestTimes = [];
 document.getElementById("item").onclick=function(){clickedTime=Date.now();
     document.getElementById("start").style.display ="none"
     reactionTime=(clickedTime-createdTime)/1000;
     createdTime=Date.now();
     var today = new Date();
+    if(StartGame===1)
     myFunction();
-    randomPlacementColorsAndShape(); 
     resetWatch();
     startWatch();
+    randomPlacementColorsAndShape(); 
+    StartGame=1;
+    
   }
 
   
   function myFunction() {
     const node = document.createElement("div");
-    const node2 = document.createElement("hr");
     const textnode = document.createTextNode("★ " + reactionTime);
     node.appendChild(textnode);
     document.getElementById("scoreboard").appendChild(node).style.textShadow ="none";
-    }
+    bestTimes[q]=reactionTime;
+    bestTimes = bestTimes.sort(function (a, b) {  return a - b;  });
+    q++;
+  }
+  function myFunction2(){
+    for(let i=0;i<=bestTimes.length-1;i++)
+  {
+    const node2 = document.createElement("div");
+    const textnode2 = document.createTextNode(i+1 + ") " + bestTimes[i]);
+    node2.appendChild(textnode2);
+    document.getElementById("bestTimes2").appendChild(node2).style.textShadow ="none";
+  }
+  
+
+  }
     function startWatch() {
         timer = setInterval(()=>{ 
           milliseconds += 10;
@@ -85,12 +101,33 @@ function randomPlacementColorsAndShape(){
 btn.addEventListener("click", function onClick(event){
 document.getElementById("scoreboard").style.display ="none";
 document.getElementById("btn2").style.display ="block";
+document.getElementById("btn4").style.display ="block";
 });
 
 btn2.addEventListener("click", function onClick(event){
     document.getElementById("scoreboard").style.display ="block";
     document.getElementById("btn2").style.display ="none";
+    document.getElementById("btn3").style.display ="none";
+    document.getElementById("btn4").style.display ="none";
     });
+btn3.addEventListener("click", function onClick(event){
+      document.getElementById("bestTimes").style.display ="none";
+      document.getElementById("btn4").style.display ="block";
+      document.getElementById("btn2").style.display ="block"
+      document.getElementById("scoreboard").style.display ="none";
+      document.querySelector("#bestTimes2").innerHTML = "";
+      
+      });
+btn4.addEventListener("click", function onClick(event){
+        document.getElementById("bestTimes").style.display ="block";
+        document.getElementById("btn3").style.display ="block";
+        document.getElementById("btn2").style.display ="none";
+        document.getElementById("btn4").style.display ="none";
+        myFunction2();
+        
+        
+        
+        });
 
 
   
